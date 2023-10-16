827b337298d7743a899e7d0e5181fd63792d0565
Kristophr Alves <kristophr.alves@puntu.digital>
2023-10-15 22:10:27 -0300

    finalizando

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ def createUser():
         cursor = mysql.cursor()
 
         # Verificar se o usuário já existe
-        cursor.execute("SELECT id FROM usuario WHERE Nome = %s", (username))
+        cursor.execute("SELECT id FROM usuario WHERE Email = %s", (email))
         existing_user = cursor.fetchone()
 
         if existing_user:
             cursor.close()
-            return jsonify({'message': 'Nome de usuário já existe'}), 400
+            return jsonify({'message': 'Email de usuário já existe'}), 400
 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
@@ -54,11 +54,11 @@ def createUser():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data['Nome']
+    email = data['Email']
     password = data['Senha']
 
     cursor = mysql.cursor()
-    cursor.execute("SELECT id, Senha FROM usuario WHERE Nome = %s", (username))
+    cursor.execute("SELECT id, Senha FROM usuario WHERE Email = %s", (email))
     user = cursor.fetchone()
 
     if user and bcrypt.check_password_hash(user[1], password):
@@ -345,6 +345,398 @@ def delete_proprietario(proprietario_id):
         return jsonify({'message': 'Proprietário excluído com sucesso'})
     except Exception as e:
         return str(e), 400
+		
+@app.route('/cadastro_animal', methods=['POST'])
+def cadastro_animal():
+    try:
+        data = request.get_json()
+        nome = data['Nome']
+        registro_marca = data['RegistroMarca']
+        especie = data['Especie']
+        raca = data['Raca']
+        sexo = data['Sexo']
+        gestacao = data['Gestacao']
+        data_nascimento = data['DataNascimento']
+        propriedade = data['Propriedade']
+        classificacao = data['Classificacao']
+        numero_cadastro_propriedade = data['NumeroCadastroPropriedade']
+        coordenadas = data['Coordenadas']
+        numero_equideos = data['NumeroEquideos']
+        cidade = data['Cidade']
+        uf = data['Uf']
+        pelagem = data['Pelagem']
+        descricao = data['Descricao']
+        proprietario_id = data['ProprietarioID']
+        usuario_id = data['UsuarioID']
+
+        cursor = mysql.cursor()
+
+        # Insira um novo animal na tabela 'Animal'
+        cursor.execute("INSERT INTO Animal (Nome, RegistroMarca, Especie, Raca, Sexo, Gestacao, DataNascimento, Propriedade, Classificacao, NumeroCadastroPropriedade, Coordenadas, NumeroEquideos, Cidade, Uf, Pelagem, Descricao, ProprietarioID, UsuarioID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                       (nome, registro_marca, especie, raca, sexo, gestacao, data_nascimento, propriedade, classificacao, numero_cadastro_propriedade, coordenadas, numero_equideos, cidade, uf, pelagem, descricao, proprietario_id, usuario_id))
+        mysql.commit()
+
+        cursor.close()
+
+        return jsonify({'message': 'Animal cadastrado com sucesso!'})
+    except Exception as e:
+        return str(e), 400
+    
+@app.route('/Getanimais/<int:usuario_id>', methods=['GET'])
+def get_animais_por_usuario(usuario_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Realize uma consulta para obter os animais associados a um usuário específico
+        cursor.execute("SELECT * FROM Animal WHERE UsuarioID = %s", (usuario_id,))
+        animais = cursor.fetchall()
+
+        cursor.close()
+
+        if animais:
+            # Crie uma lista de dicionários com as informações dos animais
+            animais_info = []
+            for animal in animais:
+                animal_info = {
+                    "ID": animal[0],
+                    "Nome": animal[1],
+                    "RegistroMarca": animal[2],
+                    "Especie": animal[3],
+                    "Raca": animal[4],
+                    "Sexo": animal[5],
+                    "Gestacao": animal[6],
+                    "DataNascimento": animal[7].strftime('%Y-%m-%d'),
+                    "Propriedade": animal[8],
+                    "Classificacao": animal[9],
+                    "NumeroCadastroPropriedade": animal[10],
+                    "Coordenadas": animal[11],
+                    "NumeroEquideos": animal[12],
+                    "Cidade": animal[13],
+                    "Uf": animal[14],
+                    "Pelagem": animal[15],
+                    "Descricao": animal[16],
+                    "ProprietarioID": animal[17],
+                    "UsuarioID": animal[18]
+                }
+                animais_info.append(animal_info)
+            return jsonify(animais_info)
+        else:
+            return jsonify({'message': 'Nenhum animal associado a este usuário'}), 404
+    except Exception as e:
+        return str(e), 400
+    
+@app.route('/animal/<int:animal_id>', methods=['PUT'])
+def update_animal(animal_id):
+    try:
+        data = request.get_json()
+        # Certifique-se de que os campos obrigatórios sejam fornecidos na solicitação JSON
+        required_fields = ["ID", "Nome", "RegistroMarca", "Especie", "Raca", "Sexo", "Gestacao", "DataNascimento", "Propriedade", "Classificacao", "NumeroCadastroPropriedade", "Coordenadas", "NumeroEquideos", "Cidade", "Uf", "Pelagem", "Descricao", "ProprietarioID", "UsuarioID"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({'message': f'O campo obrigatório {field} está faltando'}), 400
+
+        cursor = mysql.cursor()
+
+        # Atualize os dados do animal com base no ID
+        cursor.execute("UPDATE Animal SET Nome = %s, RegistroMarca = %s, Especie = %s, Raca = %s, Sexo = %s, Gestacao = %s, DataNascimento = %s, Propriedade = %s, Classificacao = %s, NumeroCadastroPropriedade = %s, Coordenadas = %s, NumeroEquideos = %s, Cidade = %s, Uf = %s, Pelagem = %s, Descricao = %s, ProprietarioID = %s, UsuarioID = %s WHERE ID = %s",
+                       (data['Nome'], data['RegistroMarca'], data['Especie'], data['Raca'], data['Sexo'], data['Gestacao'], data['DataNascimento'], data['Propriedade'], data['Classificacao'], data['NumeroCadastroPropriedade'], data['Coordenadas'], data['NumeroEquideos'], data['Cidade'], data['Uf'], data['Pelagem'], data['Descricao'], data['ProprietarioID'], data['UsuarioID'], animal_id))
+        mysql.commit()
+
+        cursor.close()
+
+        return jsonify({'message': 'Animal atualizado com sucesso'})
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/animal/<int:animal_id>', methods=['GET'])
+def get_animal_por_id(animal_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Realize uma consulta para obter um animal específico com base no ID
+        cursor.execute("SELECT * FROM Animal WHERE ID = %s", (animal_id,))
+        animal = cursor.fetchone()
+
+        cursor.close()
+
+        if animal:
+            # Crie um dicionário com as informações do animal
+            animal_info = {
+                "ID": animal[0],
+                "Nome": animal[1],
+                "RegistroMarca": animal[2],
+                "Especie": animal[3],
+                "Raca": animal[4],
+                "Sexo": animal[5],
+                "Gestacao": animal[6],
+                "DataNascimento": animal[7].strftime('%Y-%m-%d'),
+                "Propriedade": animal[8],
+                "Classificacao": animal[9],
+                "NumeroCadastroPropriedade": animal[10],
+                "Coordenadas": animal[11],
+                "NumeroEquideos": animal[12],
+                "Cidade": animal[13],
+                "Uf": animal[14],
+                "Pelagem": animal[15],
+                "Descricao": animal[16],
+                "ProprietarioID": animal[17],
+                "UsuarioID": animal[18]
+            }
+            return jsonify(animal_info)
+        else:
+            return jsonify({'message': 'Nenhum animal encontrado com o ID fornecido'}), 404
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/animal/<int:animal_id>', methods=['DELETE'])
+def delete_animal(animal_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Verifique se o animal com o ID fornecido existe
+        cursor.execute("SELECT * FROM Animal WHERE ID = %s", (animal_id,))
+        animal = cursor.fetchone()
+
+        if not animal:
+            cursor.close()
+            return jsonify({'message': 'Nenhum animal encontrado com o ID fornecido'}), 404
+
+        # Se o animal existe, execute a exclusão
+        cursor.execute("DELETE FROM Animal WHERE ID = %s", (animal_id,))
+        mysql.commit()
+        cursor.close()
+
+        return jsonify({'message': 'Animal excluído com sucesso'})
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/cadastro_exame', methods=['POST'])
+def cadastro_exame():
+    try:
+        data = request.get_json()
+        serie = data['Serie']
+        numero = data['Numero']
+        metodo = data['Metodo']
+        finalidade = data['Finalidade']
+        data_exame = data['DataExame']
+        veterinario_id = data['VeterinarioID']
+        animal_id = data['AnimalID']
+        usuario_id = data['UsuarioID']
+
+        cursor = mysql.cursor()
+
+        # Insira um novo exame na tabela 'Exame'
+        cursor.execute("INSERT INTO Exame (Serie, Numero, Metodo, Finalidade, DataExame, VeterinarioID, AnimalID, UsuarioID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                       (serie, numero, metodo, finalidade, data_exame, veterinario_id, animal_id, usuario_id))
+        mysql.commit()
+        cursor.close()
+
+        return jsonify({'message': 'Exame cadastrado com sucesso!'})
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/Getexames/<int:usuario_id>', methods=['GET'])
+def get_exames_por_usuario(usuario_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Realize uma consulta para obter todos os exames associados a um usuário específico
+        cursor.execute("SELECT * FROM Exame WHERE UsuarioID = %s", (usuario_id,))
+        exames = cursor.fetchall()
+
+        cursor.close()
+
+        if exames:
+            exames_info = []
+            for exame in exames:
+                exame_info = {
+                    "ID": exame[0],
+                    "Serie": exame[1],
+                    "Numero": exame[2],
+                    "Metodo": exame[3],
+                    "Finalidade": exame[4],
+                    "DataExame": exame[5].strftime('%Y-%m-%d'),
+                    "VeterinarioID": exame[6],
+                    "AnimalID": exame[7],
+                    "UsuarioID": exame[8]
+                }
+                exames_info.append(exame_info)
+            return jsonify(exames_info)
+        else:
+            return jsonify({'message': 'Nenhum exame associado a este usuário'}), 404
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/exame/<int:exame_id>', methods=['PUT'])
+def update_exame(exame_id):
+    try:
+        data = request.get_json()
+        serie = data['Serie']
+        numero = data['Numero']
+        metodo = data['Metodo']
+        finalidade = data['Finalidade']
+        data_exame = data['DataExame']
+        veterinario_id = data['VeterinarioID']
+        animal_id = data['AnimalID']
+        usuario_id = data['UsuarioID']
+
+        cursor = mysql.cursor()
+
+        # Atualize os dados do exame com base no ID
+        cursor.execute("UPDATE Exame SET Serie = %s, Numero = %s, Metodo = %s, Finalidade = %s, DataExame = %s, VeterinarioID = %s, AnimalID = %s, UsuarioID = %s WHERE ID = %s",
+                       (serie, numero, metodo, finalidade, data_exame, veterinario_id, animal_id, usuario_id, exame_id))
+        mysql.commit()
+        
+        cursor.close()
+
+        return jsonify({'message': 'Exame atualizado com sucesso'})
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/exame/<int:exame_id>', methods=['GET'])
+def get_exame_por_id(exame_id):
+    try:
+        cursor = mysql.cursor()
+        
+        # Realize uma consulta para obter um exame específico com base no ID
+        cursor.execute("SELECT * FROM Exame WHERE ID = %s", (exame_id,))
+        exame = cursor.fetchone()
+
+        cursor.close()
+
+        if exame:
+            exame_info = {
+                "ID": exame[0],
+                "Serie": exame[1],
+                "Numero": exame[2],
+                "Metodo": exame[3],
+                "Finalidade": exame[4],
+                "DataExame": exame[5].strftime('%Y-%m-%d'),
+                "VeterinarioID": exame[6],
+                "AnimalID": exame[7],
+                "UsuarioID": exame[8]
+            }
+            return jsonify(exame_info)
+        else:
+            return jsonify({'message': 'Nenhum exame encontrado com o ID fornecido'}), 404
+    except Exception as e:
+        return str(e), 400
+    
+@app.route('/exame/<int:exame_id>', methods=['DELETE'])
+def delete_exame(exame_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Verifique se o exame com o ID fornecido existe
+        cursor.execute("SELECT * FROM Exame WHERE ID = %s", (exame_id,))
+        exame = cursor.fetchone()
+
+        if not exame:
+            cursor.close()
+            return jsonify({'message': 'Nenhum exame encontrado com o ID fornecido'}), 404
+
+        # Se o exame existe, execute a exclusão
+        cursor.execute("DELETE FROM Exame WHERE ID = %s", (exame_id,))
+        mysql.commit()
+        cursor.close()
+
+        return jsonify({'message': 'Exame excluído com sucesso'})
+    except Exception as e:
+        return str(e), 400
+
+@app.route('/exameCompleto/<int:exame_id>', methods=['GET'])
+def get_exame_completo(exame_id):
+    try:
+        cursor = mysql.cursor()
+
+        # Realize uma consulta para obter as informações do exame com base no ID
+        cursor.execute("SELECT * FROM Exame WHERE ID = %s", (exame_id,))
+        exame = cursor.fetchone()
+
+        if not exame:
+            cursor.close()
+            return jsonify({'message': 'Nenhum exame encontrado com o ID fornecido'}), 404
+
+        # Obtenha as informações do proprietário com base no ProprietarioID do animal associado ao exame
+        cursor.execute("SELECT * FROM Proprietario WHERE ID = (SELECT ProprietarioID FROM Animal WHERE ID = %s)", (exame[7],))
+        proprietario = cursor.fetchone()
+
+        # Obtenha as informações do veterinário com base no VeterinarioID do exame
+        cursor.execute("SELECT * FROM Veterinario WHERE ID = %s", (exame[6],))
+        veterinario = cursor.fetchone()
+
+        # Obtenha as informações do animal com base no AnimalID do exame
+        cursor.execute("SELECT * FROM Animal WHERE ID = %s", (exame[7],))
+        animal = cursor.fetchone()
+
+        cursor.close()
+
+        if exame and proprietario and veterinario and animal:
+            exame_info = {
+                "Exame": {
+                    "ID": exame[0],
+                    "Serie": exame[1],
+                    "Numero": exame[2],
+                    "Metodo": exame[3],
+                    "Finalidade": exame[4],
+                    "DataExame": exame[5].strftime('%Y-%m-%d'),
+                    "VeterinarioID": exame[6],
+                    "AnimalID": exame[7],
+                    "UsuarioID": exame[8]
+                },
+                "Proprietario": {
+                    "ID": proprietario[0],
+                    "Nome": proprietario[1],
+                    "CPF": proprietario[2],
+                    "CNPJ": proprietario[3],
+                    "Endereco": proprietario[4],
+                    "Email": proprietario[5],
+                    "Telefone": proprietario[6],
+                    "Cidade": proprietario[7],
+                    "Uf": proprietario[8]
+                },
+                "Veterinario": {
+                    "ID": veterinario[0],
+                    "Nome": veterinario[1],
+                    "Cpf": veterinario[2],
+                    "Crmv": veterinario[3],
+                    "UfCrmv": veterinario[4],
+                    "Email": veterinario[5],
+                    "NumeroHabilitacao": veterinario[6],
+                    "Telefone": veterinario[7],
+                    "Endereco": veterinario[8],
+                    "Cidade": veterinario[9],
+                    "Uf": veterinario[10]
+                },
+                "Animal": {
+                    "ID": animal[0],
+                    "Nome": animal[1],
+                    "RegistroMarca": animal[2],
+                    "Especie": animal[3],
+                    "Raca": animal[4],
+                    "Sexo": animal[5],
+                    "Gestacao": animal[6],
+                    "DataNascimento": animal[7].strftime('%Y-%m-%d'),
+                    "Propriedade": animal[8],
+                    "Classificacao": animal[9],
+                    "NumeroCadastroPropriedade": animal[10],
+                    "Coordenadas": animal[11],
+                    "NumeroEquideos": animal[12],
+                    "Cidade": animal[13],
+                    "Uf": animal[14],
+                    "Pelagem": animal[15],
+                    "Descricao": animal[16],
+                    "ProprietarioID": animal[17],
+                    "UsuarioID": animal[18]
+                }
+            }
+
+            return jsonify(exame_info)
+        else:
+            return jsonify({'message': 'Alguns dados não puderam ser encontrados'}), 404
+    except Exception as e:
+        return str(e), 400
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
